f93b3402beebe92ddc9784aa48c640e32970621d
Mohammed Inshal Ansari <inshal.ansari@neosoftmail.com>
2025-01-27 08:23:43 +0530

    useCallback code updated

--- src/RN/useCallBack/CallBack.tsx
+++ src/RN/useCallBack/CallBack.tsx
@@ -1,90 +1,46 @@
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import React, { memo, useCallback, useState } from 'react';
+import React, { useState, memo, useCallback } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const initialUserList = ['Alice', 'Rose', 'Marry', 'John', 'Carry', 'Alex'];
+function useCallBack() {
+  const [count, setCount] = useState(0);
+  const [childCount, setchildCount] = useState(0);
 
-const CallBack = () => {
-  const [users, setUsers] = useState(initialUserList);
-
-  const shuffleArray = useCallback(() => {
-    const shuffledArray = [...users];
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [
-        shuffledArray[j],
-        shuffledArray[i],
-      ];
-    }
-    setUsers(shuffledArray); 
-  }, [users]);
-
-  const updateList = useCallback(
-    (value: string) => {
-      if (value === '') {
-        setUsers(initialUserList); 
-      } else {
-        const filteredUsers = initialUserList.filter((data) =>
-          data.toLowerCase().includes(value.toLowerCase()),
-        );
-        setUsers(filteredUsers); 
-      }
-    },
-    [],
-  );
+  const childfunc = useCallback(() => {
+    setchildCount((prev) => prev + 1);
+  }, [childCount]);
 
   return (
+    <View style={styles.app}>
+      <Text>{count}</Text>
+      <TouchableOpacity onPress={() => setCount((prev) => prev + 1)}>
+        <Text>Update Count</Text>
+      </TouchableOpacity>
+      <Child childCount={childCount} setchildCount={childfunc} />
+    </View>
+  );
+}
+
+const Child = memo(function Child({ childCount, setchildCount }:{childCount:number, setchildCount:()=>void}) {
+  console.log("Child Rendered");
+  return (
     <View>
-      <Search onChangeText={updateList} />
-      {users.map((value) => (
-        <Text key={value}>{value}</Text>
-      ))}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={shuffleArray}>
-        <Text style={styles.buttonText}>Shuffle</Text>
+      <Text>{childCount}</Text>
+      <TouchableOpacity onPress={setchildCount}>
+        <Text>Update Child Count</Text>
       </TouchableOpacity>
     </View>
   );
-};
-
-interface onChangeProps {
-  onChangeText: (text: string) => void;
-}
-
-const Search = memo(function SearchUser({ onChangeText }: onChangeProps) {
-  console.log('Search Rendered');
-  return (
-    <TextInput
-      style={styles.input}
-      placeholder="Enter user"
-      onChangeText={onChangeText}
-    />
-  );
 });
-
-export default CallBack;
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 10,
+  app: {
+    flex: 1,
+    backgroundColor: "brown",
   },
-  buttonText: {
-    color: '#fff',
-  },
-  input: {
-    borderWidth: StyleSheet.hairlineWidth,
-    padding: 5,
-    marginBottom: 10,
-    color: '#000',
+  text: {
+    textAlign: "center",
+    fontSize: 33,
   },
 });
+
+export default useCallBack;
